593154654715f30b39a30c2e1714280dec36efda
daniel <daniel.murden@nationwide.co.uk>
2021-07-07 16:52:14 +0100

    second commit

---
+++ Exercise1.py
@@ -0,0 +1,8 @@
+num1 = 45
+num2 = 2.5
+interger_number = int(num1)
+float_number = float(num2)
+round_number = int(round(float_number)) 
+print (num1)
+print (num2)
+print (round_number)
---
+++ tutorial_2.py
@@ -0,0 +1,8 @@
+count = 0 
+name =str(input("what is your name?"))
+
+while count < 5:
+	print (name, "is awesome")
+	count += 1
+for i in range(10,21,2):
+	Print (i)
